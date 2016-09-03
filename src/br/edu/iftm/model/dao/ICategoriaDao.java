9dff0569cc0a8a6f76734f12ee1a67539fdc5d34
carlo <carlo@DESKTOP-T5V92F4>
2016-09-03 00:52:40 -0300

    Alterações aula 03/09

---
+++ src/br/edu/iftm/model/dao/ICategoriaDao.java
@@ -0,0 +1,17 @@
+package br.edu.iftm.model.dao;
+
+import java.util.List;
+
+import br.edu.iftm.model.domain.Categoria;
+
+public interface ICategoriaDao {
+
+	void salvar(Categoria categoria);
+
+	void atualizar(Categoria categoria);
+
+	void excluir(Categoria categoria);
+
+	List<Categoria> buscar(Categoria categoria);
+
+}
